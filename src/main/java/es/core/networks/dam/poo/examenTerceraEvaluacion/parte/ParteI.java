96742f7e53f89668eef56c1f38a27426e2a3c1b6
javier <japuenca@gmail.com>
2023-05-25 19:03:02 +0200

    terminado?

--- src/main/java/es/core/networks/dam/poo/examenTerceraEvaluacion/parte/ParteI.java
+++ src/main/java/es/core/networks/dam/poo/examenTerceraEvaluacion/parte/ParteI.java
@@ -1,5 +1,122 @@
 package es.core.networks.dam.poo.examenTerceraEvaluacion.parte;
+import java.util.Scanner;
 
 public class ParteI {
+	
+	// Método 1
+    public static void cadenas() {
+        char[] nombre = {'T', 'u', ' ', 'N', 'o', 'm', 'b', 'r', 'e'};
+        char[] apellido1 = {'P', 'r', 'i', 'm', 'e', 'r', ' ', 'A', 'p', 'e', 'l', 'l', 'i', 'd', 'o'};
+        char[] apellido2 = {'S', 'e', 'g', 'u', 'n', 'd', 'o', ' ', 'A', 'p', 'e', 'l', 'l', 'i', 'd', 'o'};
+
+        // Longitud del primer apellido
+        System.out.println("Longitud del primer apellido: " + apellido1.length);
+    }
+    
+    // Método 2
+    public static void dam() {
+        String dam1 = "DAM";
+        String dam2 = new String(new char[] {'D', 'A', 'M'});
+        
+        System.out.println("Forma 1: " + dam1);
+        System.out.println("Forma 2: " + dam2);
+    }
+    
+    // Método 3
+    public static void literal() {
+        String frase = "curso de DAM";
+        int longitud = frase.length();
+        
+        System.out.println("Longitud del literal \"curso de DAM\": " + longitud);
+    }
+    
+    // Método 4
+    public static void arrayString() {
+        String texto = "Esta es la clase de POO";
+        String[] palabras = texto.split(" ");
+        
+        String concatenacion = palabras[0] + palabras[2];
+        System.out.println("Concatenación del primer y tercer elemento: " + concatenacion);
+        
+        String palabraMenor = palabras[0];
+        String palabraMayor = palabras[0];
+        
+        for (String palabra : palabras) {
+            if (palabra.compareTo(palabraMenor) < 0) {
+                palabraMenor = palabra;
+            }
+            
+            if (palabra.compareTo(palabraMayor) > 0) {
+                palabraMayor = palabra;
+            }
+        }
+        
+        System.out.println("Palabra menor: " + palabraMenor);
+        System.out.println("Palabra mayor: " + palabraMayor);
+    }
+    
+    // Método 5
+    public static void buscaEspacios(String frase) {
+        int cantidadEspacios = 0;
+        
+        for (int i = 0; i < frase.length(); i++) {
+            if (frase.charAt(i) == ' ') {
+                cantidadEspacios++;
+            }
+        }
+        
+        String mensajeSinEspacios = frase.replace(" ", "");
+        
+        System.out.println("Mensaje sin espacios: " + mensajeSinEspacios);
+        System.out.println("Cantidad de espacios en la frase: " + cantidadEspacios);
+    }
+    
+    // Método 6
+    public static void usoStringBuffer() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Ingresa tu nombre: ");
+        String nombre = scanner.nextLine();
+        
+        System.out.print("Ingresa tu primer apellido: ");
+        String apellido1 = scanner.nextLine();
+        
+        System.out.print("Ingresa tu segundo apellido: ");
+        String apellido2 = scanner.nextLine();
+        
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append(nombre).append(" ").append(apellido1).append(" ").append(apellido2);
+        
+        System.out.println("StringBuffer antes de eliminar el nombre: " + stringBuffer.toString());
+        
+        stringBuffer.delete(0, nombre.length() + 1);
+        
+        System.out.println("StringBuffer después de eliminar el nombre: " + stringBuffer.toString());
+        
+        scanner.close();
+    }
+    
+    
+    public static void main(String[] args) {
+        // Prueba del método cadenas()
+        cadenas();
+        
+        // Prueba del método dam()
+        dam();
+        
+        // Prueba del método literal()
+        literal();
+        
+        // Prueba del método arrayString()
+        arrayString();
+        
+        // Prueba del método buscaEspacios()
+        String frase = "Esta es una frase de prueba";
+        buscaEspacios(frase);
+        
+        // Prueba del método usoStringBuffer()
+        usoStringBuffer();
+    }
+
 
 }
--- src/main/java/es/core/networks/dam/poo/examenTerceraEvaluacion/parteII/ParteII.java
+++ src/main/java/es/core/networks/dam/poo/examenTerceraEvaluacion/parteII/ParteII.java
@@ -1,5 +1,136 @@
 package es.core.networks.dam.poo.examenTerceraEvaluacion.parteII;
+import java.util.HashMap;
 
 public class ParteII {
+	
+	// Parte 1
+    public static void mostrarMatriz() {
+        int[][] matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        
+        System.out.println("Matriz original:");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        } 
+    }
+    
+    public static void mostrarMatrizInvertida() {
+        int[][] matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        
+        System.out.println("Matriz invertida:");
+        for (int i = 0; i < matriz[0].length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    // Ejercicio 2
+    public static void sumaMatrices(int[][] matriz1, int[][] matriz2) {
+        if (matriz1.length != matriz2.length || matriz1[0].length != matriz2[0].length) {
+            System.out.println("No se pueden sumar las matrices. Las dimensiones no coinciden.");
+            return;
+        }
+        
+        int filas = matriz1.length;
+        int columnas = matriz1[0].length;
+        int[][] resultado = new int[filas][columnas];
+        
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                resultado[i][j] = matriz1[i][j] + matriz2[i][j];
+            }
+        }
+        
+        System.out.println("Suma de las matrices:");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(resultado[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    // Ejercicio 3
+    public static void getNumeroExtensiones() {
+        String datos = "hola.rm;usuarios.rm;test.txt;texto.rm;claves.rm;cosas.java;gatitos.rm;perritos.java;saludos.txt;adios.rm;fuego.txt;taza.java;java.txt;cuanto.txt;trece.java";
+        String[] ficheros = datos.split(";");
+        HashMap<String, Integer> extensiones = new HashMap<>();
+        
+        for (String fichero : ficheros) {
+            String[] partes = fichero.split("\\.");
+            if (partes.length == 2) {
+                String extension = partes[1];
+                extensiones.put(extension, extensiones.getOrDefault(extension, 0) + 1);
+            }
+        }
+        
+        System.out.println("Estas son las extensiones encontradas:");
+        for (String extension : extensiones.keySet()) {
+            int cantidad = extensiones.get(extension);
+            System.out.println(extension + ": " + cantidad);
+        }
+    }
+    
+    // Ejercicio 4
+    public interface Estadisticas {
+        double getArea();
+        double getPerimetro();
+    }
 
+    public abstract class FiguraGeometrica implements Estadisticas {
+        public abstract String getName();
+    }
+
+    public class Circulo extends FiguraGeometrica {
+        private double radio;
+        
+        public Circulo(double radio) {
+            this.radio = radio;
+        }
+        
+        @Override
+        public String getName() {
+            return "Círculo";
+        }
+        
+        @Override
+        public double getArea() {
+            return Math.PI * radio * radio;
+        }
+        
+        @Override
+        public double getPerimetro() {
+            return 2 * Math.PI * radio;
+        }
+    }
+
+    public class Cuadrado extends FiguraGeometrica {
+        private double lado;
+        
+        public Cuadrado(double lado) {
+            this.lado = lado;
+        }
+        
+        @Override
+        public String getName() {
+            return "Cuadrado";
+        }
+        
+        @Override
+        public double getArea() {
+            return lado * lado;
+        }
+        
+        @Override
+        public double getPerimetro() {
+            return 4 * lado;
+        }
+    }
+    
 }
+
+
